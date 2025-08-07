7992da39167c9cf285c9ec8a894e63acb3bf03b4
rajnishsharma2601 <kumarrajnish2600@gmail.com>
2025-08-07 15:37:53 +0530

    updtate in node

--- rajnish/backend/app.js
+++ rajnish/backend/app.js
@@ -1,13 +1,12 @@
-import cookieParser from 'cookie-parser';
+import { config } from "dotenv";
 config();
-import express from 'express';
-import { config } from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
-import errorMiddleware from './middlewares/error.middleware.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
-
 // Middlewares
 // Built-In
 app.use(express.json());
@@ -19,28 +18,28 @@ app.use(
     credentials: true,
   })
 );
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
 // Server Status Check Route
-app.get('/ping', (_req, res) => {
-  res.send('Pong');
+app.get("/ping", (_req, res) => {
+  res.send("Pong");
 });
 
 // Import all routes
-import userRoutes from './routes/user.routes.js';
-import courseRoutes from './routes/course.routes.js';
-import paymentRoutes from './routes/payment.routes.js';
-import miscRoutes from './routes/miscellaneous.routes.js';
+import userRoutes from "./routes/user.routes.js";
+import courseRoutes from "./routes/course.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import miscRoutes from "./routes/miscellaneous.routes.js";
 
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1', miscRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/courses", courseRoutes);
+app.use("/api/v1/payments", paymentRoutes);
+app.use("/api/v1", miscRoutes);
 
 // Default catch all route - 404
-app.all('*', (_req, res) => {
-  res.status(404).send('OOPS!!! 404 Page Not Found');
+app.all("*", (_req, res) => {
+  res.status(404).send("OOPS!!! 404 Page Not Found");
 });
 
 // Custom error handling middleware
