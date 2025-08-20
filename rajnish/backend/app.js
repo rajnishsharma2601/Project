0d8bcbb80355b4db1fb55ec214682e7d3ccb18c0
rajnishsharma2601 <kumarrajnish2600@gmail.com>
2025-08-20 20:37:20 +0530

    update in app.js

--- rajnish/backend/app.js
+++ rajnish/backend/app.js
@@ -38,8 +38,8 @@ app.use("/api/v1/payments", paymentRoutes);
 app.use("/api/v1", miscRoutes);
 
 // Default catch all route - 404
-app.all("*", (_req, res) => {
-  res.status(404).send("OOPS!!! 404 Page Not Found");
+app.all("/", (_req, res) => {
+  res.status(404).send(" hello world");
 });
 
 // Custom error handling middleware
