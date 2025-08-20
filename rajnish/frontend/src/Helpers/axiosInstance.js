f30fabd776841ad3d04dfe225ad0018a84857140
rajnishsharma2601 <kumarrajnish2600@gmail.com>
2025-08-20 21:13:49 +0530

    update in app.js

--- rajnish/frontend/src/Helpers/axiosInstance.js
+++ rajnish/frontend/src/Helpers/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-  const BASE_URL = "http://localhost:5000/api/v1";
-// const BASE_URL = "https://lmsbackend-gray.vercel.app/api/v1";
+  //const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://backend-ten-phi-86.vercel.app/api/v1";
+ //https://backend-ten-phi-86.vercel.app/ 
 
 const axiosInstance = axios.create();
 
